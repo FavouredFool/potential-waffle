3fe765d2669a00682c57c87ba9185f8ededaccd1
Tim Wahrburg <t.wahrburg@gmail.com>
2023-09-30 21:03:25 +0200

    Bits fade out

--- potential-waffle/Assets/Main/Script/PlanetBit.cs
+++ potential-waffle/Assets/Main/Script/PlanetBit.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlanetBit : MonoBehaviour
 {
+    [SerializeField] float _timeTillKill = 15f;
+
     Rigidbody2D _rigidBody;
 
     void Start()
@@ -15,5 +18,8 @@ public class PlanetBit : MonoBehaviour
         Vector3 force = direction * strength;
 
         _rigidBody.AddForce(force, ForceMode2D.Impulse);
+
+        // Scale down and then destroy the laser
+        transform.DOScale(Vector3.zero, _timeTillKill).SetEase(Ease.InCubic).OnComplete(()=> Destroy(this.gameObject));
     }
 }
