2d796b9d56ac2f824e46eaee68cfac2c014ce535
Tim Wahrburg <t.wahrburg@gmail.com>
2023-09-30 11:17:53 +0200

    spinning with style

---
+++ potential-waffle/Assets/Main/Script/ShipMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShipMovement : MonoBehaviour
+{
+    
+    [SerializeField] float _moveSpeed = 2;
+    [SerializeField] float _rotateSpeed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, Time.deltaTime * _moveSpeed);
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * _rotateSpeed));
+    }
+}
