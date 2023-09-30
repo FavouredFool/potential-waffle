7a1d8c579ce0cc7f3ea9c09bf131952b474ce866
Tim Wahrburg <t.wahrburg@gmail.com>
2023-09-30 20:19:16 +0200

    planets go puff

---
+++ potential-waffle/Assets/Main/Script/Planet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Shapes;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] int _maxHealth;
+    [SerializeField] Disc _healthbarDisc;
+    [SerializeField] Gradient _healthbarGradient;
+
+    int _currentHealth;
+
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _currentHealth--;
+        }
+
+        if (_currentHealth <= 0)
+        {
+            // Kill, spawn planetbits and resources
+            Destroy(this.gameObject);
+            return;
+        }
+        
+        float healthPercent = _currentHealth / (float)_maxHealth;
+        _healthbarDisc.AngRadiansEnd = healthPercent * 2 * Mathf.PI + Mathf.PI/2;
+
+        Color healthbarColor = _healthbarGradient.Evaluate(healthPercent);
+        healthbarColor.a = 0.1f;
+        _healthbarDisc.Color = healthbarColor;
+    }
+
+
+}
