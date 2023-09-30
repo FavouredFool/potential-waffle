7f49b0794cbb6438c7d2594a8a4004156dd5d769
Tim Wahrburg <t.wahrburg@gmail.com>
2023-09-30 22:11:29 +0200

    ship is rock solid

--- potential-waffle/Assets/Main/Script/ShipMovement.cs
+++ potential-waffle/Assets/Main/Script/ShipMovement.cs
@@ -5,18 +5,22 @@ using DG.Tweening;
 
 public class ShipMovement : MonoBehaviour
 {
-    
     [SerializeField] float _moveSpeed = 2;
     [SerializeField] float _rotateSpeed = 10;
 
+    Rigidbody2D _rigidbody;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, Time.deltaTime * _moveSpeed);
-        transform.Rotate(new Vector3(0, 0, Time.deltaTime * _rotateSpeed));
+        //transform.RotateAround(Vector3.zero, Vector3.forward, Time.deltaTime * _moveSpeed);
+        //transform.Rotate(new Vector3(0, 0, Time.deltaTime * _rotateSpeed));
+
+        Quaternion q = Quaternion.AngleAxis(_moveSpeed * Time.deltaTime, transform.forward);
+        _rigidbody.MovePosition(q * _rigidbody.transform.position);
     }
 }
