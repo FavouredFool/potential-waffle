e9c7965510a79184704939a855c1157cb7458f32
Tim Wahrburg <t.wahrburg@gmail.com>
2023-10-01 13:11:29 +0200

    go brrr

--- potential-waffle/Assets/Main/Script/Gun.cs
+++ potential-waffle/Assets/Main/Script/Gun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -7,12 +8,21 @@ public class Gun : MonoBehaviour
     [SerializeField] GameObject _laserBlueprint; 
     [SerializeField] Transform _shootingPointTransform;
     [SerializeField] float _fireRatePerSecond = 1;
+    [SerializeField] float _rotateSpeed = 2;
 
     float _timeLastShot = float.NegativeInfinity;
+
+    Vector2 _gunLookDirection = Vector2.up;
 
     void Update()
     {
         ShootLaser();
+    }
+
+    void FixedUpdate()
+    {
+        _gunLookDirection = Quaternion.Euler(0, 0, -_rotateSpeed) * _gunLookDirection;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, (Vector3)_gunLookDirection);
     }
 
     void ShootLaser()
