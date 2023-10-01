ca9781243df55c1a82673a0eadc8a885c24c386a
Tim Wahrburg <t.wahrburg@gmail.com>
2023-10-01 11:15:37 +0200

    i invented magnetism

--- potential-waffle/Assets/Main/Script/ResourceCollectable.cs
+++ potential-waffle/Assets/Main/Script/ResourceCollectable.cs
@@ -6,6 +6,32 @@ using static ResourceManager;
 public class ResourceCollectable : MonoBehaviour
 {
     [SerializeField] ResourceType _type;
+    [SerializeField] float _magnetRadius;
+    [SerializeField] float _magnetForce;
+
+    PlayerMovement _player;
+    Rigidbody2D _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+
+        if (_player == null)
+        {
+            Debug.LogError("NO PLAYER FOUND!");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 toPlayer = (Vector2)(_player.transform.position - transform.position);
+
+        if (toPlayer.magnitude < _magnetRadius)
+        {
+            _rigidBody.AddForce(toPlayer.normalized * _magnetForce);
+        }
+    }
 
     public ResourceType GetResourceType()
     {
