259e8aea843e6f400e87a8e284fd3aed501e7918
Tim Wahrburg <t.wahrburg@gmail.com>
2023-10-02 19:28:46 +0200

    stuff

--- potential-waffle/Assets/Main/Script/ShipMovement.cs
+++ potential-waffle/Assets/Main/Script/ShipMovement.cs
@@ -45,7 +45,7 @@ public class ShipMovement : MonoBehaviour
             // Does the station explode?
 
             
-            for (int i = 0; i < Random.Range(100, 200); i++) 
+            for (int i = 0; i < Random.Range(60, 70); i++) 
             {
                 Instantiate(_remainingElementBlueprint, transform.position, transform.rotation);
             }
